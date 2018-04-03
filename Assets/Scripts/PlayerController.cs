0e8b581ac318b26319e2452cdbe23c8a6970a0c1
Albin Wejros <albin.wejros@gmail.com>
2018-04-03 17:46:37 +0200

    Added physics

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	float tilt = 20.0f;
+	float smooth = 5.0f;
+	// Use this for initialization
+	void Start () {
+		Update ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float tiltZ = Input.GetAxis ("Horizontal") * tilt;
+		float tiltX = Input.GetAxis ("Vertical") * tilt;
+
+		Quaternion target = Quaternion.Euler (tiltX, 0, tiltZ);
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, target,  Time.deltaTime * smooth);
+	}
+}
