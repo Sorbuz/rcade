e14677f664abe9ac194fb2ec370edfafdfc69d7d
Albin Wejros <albin.wejros@gmail.com>
2018-04-09 16:29:06 +0200

    Added game logics, win conditions and timer

--- Assets/Scripts/Marble.cs
+++ Assets/Scripts/Marble.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Marble : MonoBehaviour {
+	public Text timer;
+	private float time;
+	private bool timerRunning;
+
 	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		timerRunning = false;
 	}
 	
 	// Update is called once per frame
@@ -14,6 +20,22 @@ public class Marble : MonoBehaviour {
 		// Press space to start
 		if (Input.GetKey (KeyCode.Space)) {
 			rb.useGravity = true;
+			timerRunning = true;
+		}
+
+		if (timerRunning) {
+			time += Time.deltaTime;
+		}
+
+		decimal d1 = (decimal)time;
+		decimal rounded = decimal.Round (d1, 1);
+		timer.text = "Time: " + rounded.ToString () + " seconds";
+	}
+
+	void OnTriggerEnter(Collider other) {
+		// Won game
+		if (other.gameObject.CompareTag ("Finish")) {
+			this.gameObject.SetActive (false);
 		}
 	}
 }
