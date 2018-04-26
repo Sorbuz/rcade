bdc5c22159074078be849a249879a5d5d57d8433
Albin Wejros <albin.wejros@gmail.com>
2018-04-27 01:14:09 +0200

    Multiple asteroids now spawn

---
+++ Assets/Scripts/Interstellar Blast Scripts/DestroyLoseText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyLoseText : MonoBehaviour {
+	void Update () {
+		if (transform.position.z < -10) {
+			Destroy (gameObject);
+		}
+	}
+}
--- Assets/Scripts/Interstellar Blast Scripts/GameController.cs
+++ Assets/Scripts/Interstellar Blast Scripts/GameController.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-	public GameObject hazard;
+	public GameObject asteroid;
+	public GameObject loseText;
 	public Vector3 spawnValues;
+	public float timeBetweenSpawns;
+	public bool spawning = true;
 
-
-	void SpawnWaves () {
-		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-5, 8), spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard, spawnPosition, spawnRotation);
-	}
 
 	void Start () {
-		SpawnWaves ();
+		StartCoroutine(SpawnWaves ());
+	}
+
+	IEnumerator SpawnWaves () {
+		while (spawning) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), Random.Range (-5, 8), spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (asteroid, spawnPosition, spawnRotation);
+			yield return new WaitForSeconds (timeBetweenSpawns);
+
+			// Stops the asteroid spawnings
+			if (!GameObject.Find("Player")) {
+				spawning = false;
+				Instantiate (loseText, new Vector3 (0, 0, 500), new Quaternion());
+			}
+		}
 	}
 }
